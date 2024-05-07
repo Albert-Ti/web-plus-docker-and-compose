c7d213dc6c7719b4f7b0154789f5f5e59003caa1
Albert Taygibov <albert.ti.2017@yandex.ru>
2024-05-08 01:36:54 +0300

    fix: add domain

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,12 @@ import helmet from 'helmet';
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:8081' });
+  app.enableCors({
+    origin: [
+      'https://kupipodariday.albert.nomoredomainswork.ru',
+      'http://kupipodariday.albert.nomoredomainswork.ru',
+    ],
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
